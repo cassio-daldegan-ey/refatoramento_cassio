5609b4c630cfbdffd30bfc8cb847e9a96c1ffa54
cassio-daldegan-ey <Cassio.Daldegan@br.ey.com>
2025-02-18 13:52:59 -0300

    Delete model directory

--- model/__init__.py
+++
@@ -1,7 +0,0 @@
-# Define a variável __all__
-__all__ = ["train_model","evaluate_model","predict_model"]
-
-# Importa os seguintes submódulos
-from . import train_model
-from . import evaluate_model
-from . import predict_model
