6cdc66345b42ccd8a55d34b8c104985ce905171f
cassio-daldegan-ey <Cassio.Daldegan@br.ey.com>
2025-02-18 13:54:03 -0300

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+# Define a variável __all__
+__all__ = ["train_model","evaluate_model","predict_model"]
+
+# Importa os seguintes submódulos
+from . import train_model
+from . import evaluate_model
+from . import predict_model
